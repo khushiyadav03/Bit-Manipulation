dffaf7d70c2bd73655bfa1e0053be6a09db8039f
Khushi Yadav <ky0307005@gmail.com>
2026-01-21 21:19:33 +0530

    Initial commit

---
+++ Bit Manipulation/swap two numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a , b;
+    cin >> a >> b;
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    cout << "After swapping: " << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    return 0;
+}
